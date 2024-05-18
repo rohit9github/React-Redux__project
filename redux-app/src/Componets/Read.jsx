f4e0327ef750d273f5c667c14d0aefbf48747a1a
gohil rohit <gohilrohit309@gmail.com>
2024-05-18 18:53:04 +0530

    filter data on gender

--- redux-app/src/Componets/Read.jsx
+++ redux-app/src/Componets/Read.jsx
@@ -5,16 +5,18 @@ import Container from 'react-bootstrap/Container';
 import { useDispatch, useSelector } from "react-redux";
 import { deleteUser, showUser } from "../features/userDetailsslice";
 import Modal from "./Modal";
+import Form from 'react-bootstrap/Form';
 import { useNavigate } from "react-router-dom";
 
 function Read() {
 
     const dispatch = useDispatch();
-    const[id,setId] =useState();
-    const [showModal,setShowModal] = useState(false);
-    const navigate = useNavigate()
+    const [id, setId] = useState();
+    const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate();
+    const[radioData,setRadioData] = useState("");
 
-    const { users, loading,searchData } = useSelector((state) => state.app)
+    const { users, loading, searchData } = useSelector((state) => state.app)
 
     useEffect(() => {
 
@@ -36,29 +38,42 @@ function Read() {
             {showModal && <Modal id={id} showModal={showModal} setShowModal={setShowModal} />}
             <h1>Read</h1>
             <Container>
+                <Form.Check type="radio" label="All" name='gender' checked={radioData==""} value={""}  onChange={(e)=>setRadioData(e.target.value)} />
+                <Form.Check type="radio" label="Male" name='gender' value="male" onChange={(e)=>setRadioData(e.target.value)} />
+                <Form.Check type="radio" label="Female" name='gender' value="female" onChange={(e)=>setRadioData(e.target.value)} />
                 {users &&
-                    users.filter((v,i)=>{
-                        if(searchData.length === 0){
+                    users.filter((v, i) => {
+                        if (searchData.length === 0) {
                             return v
                         }
-                        else{
+                        else {
                             return v.name.toLowerCase().includes(searchData.toLowerCase())
                         }
+                    }).filter((v1)=>{
+                        if(radioData === "male"){
+                            return v1.gender === radioData
+                        }
+                        else if (radioData === "female" ){
+                            return v1.gender === radioData
+                        }
+                        else{
+                            return v1
+                        }
                     })
-                .map((v, i) => (
-                   
-                        <Card style={{ width: '24rem' }} key={i}>
-                            <Card.Body > 
-                                <Card.Title>{v.name}</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">{v.gender}</Card.Subtitle>
-                                <Card.Text>{v.mail}</Card.Text>
-                                <button onClick={()=>[setId(v.id),setShowModal(true)]}>View</button> || 
-                                 <button onClick={()=>updateUser(v.id)}>Edit</button> || 
-                                <button onClick={()=>dispatch(deleteUser(v.id))}>Delete</button>
-                            </Card.Body>
-                        </Card>
-                    
-                ))}
+                        .map((v, i) => (
+
+                            <Card style={{ width: '24rem' }} key={i}>
+                                <Card.Body >
+                                    <Card.Title>{v.name}</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">{v.gender}</Card.Subtitle>
+                                    <Card.Text>{v.mail}</Card.Text>
+                                    <button onClick={() => [setId(v.id), setShowModal(true)]}>View</button> ||
+                                    <button onClick={() => updateUser(v.id)}>Edit</button> ||
+                                    <button onClick={() => dispatch(deleteUser(v.id))}>Delete</button>
+                                </Card.Body>
+                            </Card>
+
+                        ))}
 
             </Container>
         </div>
